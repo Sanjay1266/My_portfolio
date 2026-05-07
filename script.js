d567e202ec66b187adb488d3c60722a1140a807a
Sanjay1266 <sanjaysundaram50@gmail.com>
2026-05-07 10:32:50 +0530

    Contact form completed

--- script.js
+++ script.js
@@ -394,4 +394,35 @@
       }
     });
   }
+  /* ================= CONTACT FORM ================= */
+
+  const contactForm = document.getElementById("contact-form");
+
+  if (contactForm) {
+    contactForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(contactForm);
+
+      try {
+        const response = await fetch(contactForm.action, {
+          method: "POST",
+          body: formData,
+          headers: {
+            Accept: "application/json",
+          },
+        });
+
+        if (response.ok) {
+          alert("Message sent successfully!");
+
+          contactForm.reset();
+        } else {
+          alert("Failed to send message.");
+        }
+      } catch (error) {
+        alert("Something went wrong!");
+      }
+    });
+  }
 })();
